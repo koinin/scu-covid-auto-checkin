c70ecd6a5c4258e5df59cd49eaa0001f135c08a0
Hugh <1728902978@qq.com>
2022-02-28 00:54:06 +0800

    [0.9] need to check the flag only

--- checkin.py
+++ checkin.py
@@ -8,21 +8,21 @@ import time
 import requests
 
 
-def generate_cookies_dict() -> dict:
-    try:
-        cookies_dict = {
-            'eai-sess': os.environ['EAI_SESS'],
-            'UUkey': os.environ['UUKEY']
-        }
-        return cookies_dict
-    except Exception as ept:
-        print(f'[ERROR] 运行错误：{ept}')
-        exit()
+pat_info = re.compile('var def =(.*);!?')
+pat_fin = re.compile("hasFlag: '(\d)',")
+sess = None
+key = None
+campus = None
 
+def generate_cookies_dict() -> dict:
+    return {
+        'eai-sess': sess,
+        'UUkey': key
+    }
 
 def modify_json(res_json: dict) -> dict:
     # load default geo_api_info
-    with open(os.path.join('resource', f'{os.environ["CAMPUS"]}.json'), 'r') as ifile:
+    with open(os.path.join('resource', f'{campus}.json'), 'r') as ifile:
         res_json['geo_api_info'] = json.load(ifile)
 
     res_json['province'] = res_json['geo_api_info']['addressComponent']['province']
@@ -39,7 +39,7 @@ def modify_json(res_json: dict) -> dict:
     return res_json
 
 
-def checkin(cookies_dict: dict):
+def checkin(cookies_dict: dict)->bool:
     # base data
     session = requests.session()
     url = 'https://wfw.scu.edu.cn/ncov/wap/default/index'
@@ -49,14 +49,20 @@ def checkin(cookies_dict: dict):
     resp = session.get(url=url)
     if resp.status_code != 200:
         print('[ERROR]', resp.status_code)
-        exit()
+        return False
 
     html = resp.content.decode('utf-8')
-    pattern = re.compile('var def =(.*);!?')
-    res = re.findall(pattern, html)
+
+    status = pat_fin.findall(html)[0]
+    if status=="1": # had checkin
+        print("[ERROR] already checkin today")
+        session.close()
+        return False
+
+    res = pat_info.findall(html)
     if len(res) == 0:
         print('[ERROR] not found')
-        exit()
+        return False
     res_json = json.loads(res[0])
     
     # load geo info & modify data
@@ -67,10 +73,26 @@ def checkin(cookies_dict: dict):
     resp = session.post(url=url, data=res_json)
     if resp.status_code == 200:
         resp_json = json.loads(resp.content.decode('utf-8'))
-        print(f'[INFO] 签到结果：{resp_json["m"]}')
+        print(f'[INFO] 签到结果:{resp_json["m"]}')
+        return True
     else:
-        print(f'[ERROR] 签到失败：{resp.status_code} {resp.content.decode("utf-8")}')
+        print(f'[ERROR] 签到失败:{resp.status_code} {resp.content.decode("utf-8")}')
+        return False
+
+def all_checkin(file:str):
+    with open(file) as fd:
+        data = json.loads(fd.read())
+    for person in data:
+        global campus
+        campus=person["CAMPUS"]
+        if checkin({
+            'eai-sess': person["EAI_SESS"],
+            'UUkey': person["UUKEY"]
+        }):
+            print(person["name"]+" has checkin")
+        else:
+            print(person["name"]+" failed to checkin")
 
 
 if __name__ == '__main__':
-    checkin(generate_cookies_dict())
+    all_checkin("resource/people.json")
